f5564596f27930328a3e52e286c984e8980c405d
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-03-06 17:33:00 -0300

    :sparkles: initial settings

--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -1,8 +1,10 @@
 import styles from './styles.module.scss';
+import IgniteSvg from '../assets/ignite.svg';
 
 export const Header = () => {
   return (
     <header className={styles.header}>
+      <img src={IgniteSvg} alt="ignite-logo" />
       <strong>Ignite Feed</strong>
     </header>
   );
